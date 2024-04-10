6e5c7c5050ae14d480e30d592dc59cf637350102
syed <s.muazzam.siddiqhi@gmail.com>
2024-04-10 05:02:39 +0200

    [MV-ASSIGNMENT] - Add Movie repository, representing database design and data readiness.

--- src/main/java/com/etrg/syed/assignment/movierental/repository/MovieRepository.java
+++ src/main/java/com/etrg/syed/assignment/movierental/repository/MovieRepository.java
@@ -3,5 +3,28 @@
  */
 package com.etrg.syed.assignment.movierental.repository;
 
+import com.etrg.syed.assignment.movierental.model.Movie;
+import com.etrg.syed.assignment.movierental.model.MovieTypeEnum;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class MovieRepository {
+    //Mock MovieRepository using Map, since there is no database connected at the moment.
+    private final Map<String, Movie> movies = new HashMap<>();
+
+    //simulating datatbase operation using map, as there is no JpaRepository, we interact direct with map.
+    public MovieRepository() {
+        //populate the map with some movies
+        movies.put("F001", new Movie("You've Got Mail", MovieTypeEnum.REGULAR));
+        movies.put("F002", new Movie("Matrix", MovieTypeEnum.NEW_RELEASE));
+        movies.put("F003", new Movie("Cars", MovieTypeEnum.CHILDRENS));
+        //Can add more moies
+    }
+
+    //Implementing findById similar to Jpa. Can be moved away easily from map, wraps the result in an Optional for better null handling.
+    public Optional<Movie> findById(String id) {
+        return Optional.ofNullable(movies.get(id));
+    }
 }
